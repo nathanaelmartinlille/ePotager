b7c5995ca28aa600d65836e80b9e314825f19859
nath <nath@172.19.131.88>
2013-11-21 16:33:49 +0100

    test

---
+++ src/com/example/potago/ProfilActivity.java
@@ -0,0 +1,22 @@
+package com.example.potago;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ProfilActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_profil);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.profil, menu);
+		return true;
+	}
+
+}
