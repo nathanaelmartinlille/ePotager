74b100b16e5ecacac85bcc6c46ed65dea7c9982e
Laura Leclercq <leclercq.laura@gmail.com>
2013-12-31 11:59:41 +0100

    Slider géolocalisation

--- src/com/example/potago/Geolocalisation.java
+++ src/com/example/potago/Geolocalisation.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,6 +29,22 @@ public class Geolocalisation extends Activity{
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_geoloc);
 	    
+	    ImageButton back = (ImageButton)this.findViewById(R.id.boutonBack);
+		back.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+		
+		ImageButton home = (ImageButton)this.findViewById(R.id.boutonHome);
+		home.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+	    
 		TextView titre = (TextView)this.findViewById(R.id.titre);
 		titre.setText("Géolocalisation");
 	    
@@ -48,22 +66,43 @@ public class Geolocalisation extends Activity{
 	    // Zoom in, animating the camera.
 	   // map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
 	    
+	    // On récupère les éléments
+	    final TextView rayon = (TextView)this.findViewById(R.id.rayon);
+	    final SeekBar distance = (SeekBar)this.findViewById(R.id.distance);
 	    
-	    ImageButton back = (ImageButton)this.findViewById(R.id.boutonBack);
-		back.setOnClickListener(new View.OnClickListener() {
+	    // Pour commencer, on met le slide au milieu
+	    distance.setProgress(50);
+	    rayon.setText("50km");
+	    
+	    // Ici on gère les conditions
+	    distance.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+	    	/**
+	    	 * Quand on a fini de changer la distance du slide, on recharge les données.
+	    	 */
 			@Override
-			public void onClick(View v) {
-				finish();
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				rayon.setText(seekBar.getProgress() + "km");
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			/**
+			 * Quand on change la valeur de la distance, on ne fait que changer le texte.
+			 * On ne recharge pas tout de suite les données, sinon il y aurait trop de requêtes.
+			 */
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				rayon.setText(seekBar.getProgress() + "km");
+				System.out.println("progress : "+seekBar.getProgress());
 			}
 		});
-		
-		ImageButton home = (ImageButton)this.findViewById(R.id.boutonHome);
-		home.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				finish();
-			}
-		});
+
 	  }
 
 
